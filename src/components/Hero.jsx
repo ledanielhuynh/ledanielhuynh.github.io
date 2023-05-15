8f792dba253794c08edb59a99945684496131f90
Daniel <ledanielhuynh@gmail.com>
2023-05-15 13:49:50 +1000

    added in typewriter-effect to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import danielhello from '../assets/danielhello.png';
+import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
   return (
     <div className='flex md:flex-row flex-col space-x-4 justify-center items-center md:px-16 px-12 max-w-screen-xl bg-zinc-900 rounded-2xl'>
       <div className='flex flex-col space-y-8 md:py-12 py-8 md:w-2/3 w-full'>
         <div className='text-6xl text-white font-bold'>
-          <h1>HEY,</h1>
+          <div className='flex flex-row'>
+            <Typewriter
+              options={{
+                strings: ['HELLO', 'XIN CHÀO', '안녕하세요', 'こんにちは'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+            <p>,</p>
+          </div>          
           <h1>DANIEL HERE!</h1>
         </div>
         <ul className='md:text-xl text-lg text-zinc-300'> I am currently a Computer Science student at UNSW who is aspiring to become a unicorn developer! In saying 
